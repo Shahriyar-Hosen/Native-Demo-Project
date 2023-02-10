6b65fa29ca34a0c4468eff936a576cc19f788b91
Shahriyar-Hosen <temp.intern.shahriyar.nov.2@inmogr.com>
2023-02-10 17:40:53 +0600

    setup folder stacture

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -7,12 +7,9 @@ const WelcomeScreen = () => {
   return (
     <ImageBackground
       style={styles.background}
-      source={require('../../assets/background.jpg')}>
+      source={require('../assets/background.jpg')}>
       <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require('../../assets/logo-red.png')}
-        />
+        <Image style={styles.logo} source={require('../assets/logo-red.png')} />
         <Text style={styles.text}>Sel What You DOn't Need</Text>
       </View>
       <View style={styles.loginButton} />
