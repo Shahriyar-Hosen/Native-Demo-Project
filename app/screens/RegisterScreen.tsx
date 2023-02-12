e7859fb1f67f9f1286791cf852a75128c8c2cc85
Shahriyar-Hosen <temp.intern.shahriyar.nov.2@inmogr.com>
2023-02-12 22:06:29 +0600

    RegisterScreen complete

--- app/screens/RegisterScreen.tsx
+++ app/screens/RegisterScreen.tsx
@@ -1,35 +1,45 @@
 import {Formik} from 'formik';
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import * as Yup from 'yup';
-import Screen from '../Components/common/Screen';
+import {Screen} from '../Components/common';
 import {AppFormField, SubmitButton} from '../Components/forms';
 
-interface LoginFormProps {
+interface RegisterFormProps {
+  name: string;
   email: string;
   password: string;
 }
 
 const VALIDATION_SCHEMA = Yup.object().shape({
+  name: Yup.string().required().label('Name'),
   email: Yup.string().required().email().label('Email'),
   password: Yup.string().required().min(4).label('Password'),
 });
 
 const RegisterScreen = () => {
-  const initialValues: LoginFormProps = {
+  const initialValues: RegisterFormProps = {
+    name: '',
     email: '',
     password: '',
   };
 
   return (
     <Screen style={styles.container}>
-      <Image style={styles.logo} source={require('../assets/logo-red.png')} />
       <Formik
         initialValues={initialValues}
         validationSchema={VALIDATION_SCHEMA}
         onSubmit={values => console.log(values)}>
         {({errors, touched}) => (
           <>
+            <AppFormField
+              autoCorrect={false}
+              icon="account"
+              name="name"
+              placeholder="Name"
+              error={errors.name}
+              visible={touched.name}
+            />
             <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
@@ -52,7 +62,7 @@ const RegisterScreen = () => {
               error={errors.password}
               visible={touched.password}
             />
-            <SubmitButton title="Login" />
+            <SubmitButton title="Register" />
           </>
         )}
       </Formik>
@@ -62,17 +72,7 @@ const RegisterScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    padding: '2.5%',
-  },
-  logo: {
-    width: 80,
-    height: 80,
-    alignSelf: 'center',
-    marginTop: 50,
-    marginBottom: 20,
-  },
-  error: {
-    color: 'red',
+    padding: 10,
   },
 });
 
