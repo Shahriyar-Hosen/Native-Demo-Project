4249458cc61c765d7a71dc4180177b6c9ce6b32d
Shahriyar-Hosen <temp.intern.shahriyar.nov.2@inmogr.com>
2023-02-14 00:11:37 +0600

    Nesting Navigators

--- App.tsx
+++ App.tsx
@@ -1,30 +1,36 @@
 /**
  * @format
  */
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  NavigationContainer,
+  ParamListBase,
+  RouteProp,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import {Button} from 'react-native';
 import {AppText, Screen} from './app/Components/common';
-
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      title="View Tweet"
-      onPress={() => navigation.navigate('TweetsDetails')}
-    />
-  );
-};
 
 const Tweets = ({navigation}) => (
   <Screen>
     <AppText>Tweets</AppText>
-    <Link />
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate('TweetsDetails', {id: 513})}
+    />
   </Screen>
 );
 
-const TweetsDetails = () => (
+const TweetsDetails = ({route}) => (
+  <Screen>
+    <AppText>TweetsDetails {route.params.id}</AppText>
+  </Screen>
+);
+
+const Account = () => (
   <Screen>
     <AppText>TweetsDetails</AppText>
   </Screen>
@@ -39,10 +45,53 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  const tabBarIcons = ({
+    route,
+    focused,
+    color,
+    size,
+  }: {
+    route: RouteProp<ParamListBase, string>;
+    focused: boolean;
+    color: string;
+    size: number;
+  }): JSX.Element => {
+    let iconName =
+      (route.name === 'Feed'
+        ? focused
+          ? 'home'
+          : 'home-outline'
+        : focused
+        ? 'account'
+        : 'account-outline') || '';
+
+    return <Icon name={iconName} size={size} color={color} />;
+  };
+
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarActiveBackgroundColor: 'tomato',
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveBackgroundColor: '#eee',
+        tabBarInactiveTintColor: 'black',
+        tabBarIcon: ({focused, color, size}) =>
+          tabBarIcons({route, focused, color, size}),
+        headerShown: false,
+      })}>
+      <Tab.Screen name="Feed" component={StackNavigator} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+};
+
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 };
