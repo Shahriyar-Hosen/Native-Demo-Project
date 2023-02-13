407444c12afe6f5ff5cc2f99512dfae8c6192bad
Shahriyar-Hosen <temp.intern.shahriyar.nov.2@inmogr.com>
2023-02-13 22:57:14 +0600

    Navigating Between Screens

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 /**
  * @format
  */
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import {Button} from 'react-native';
+import {AppText, Screen} from './app/Components/common';
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate('TweetsDetails')}
+    />
+  );
+};
+
+const Tweets = ({navigation}) => (
+  <Screen>
+    <AppText>Tweets</AppText>
+    <Link />
+  </Screen>
+);
+
+const TweetsDetails = () => (
+  <Screen>
+    <AppText>TweetsDetails</AppText>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
+  </Stack.Navigator>
+);
 
 const App = () => {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
